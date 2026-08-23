29d3137eeff82cfb41edb0dd02fb4c2f0678fcf4
sahakyanararat646-design <sahakyanararat646@gmail.com>
2026-08-23 20:47:42 +0400

    Update app.py

--- app.py
+++ app.py
@@ -62,9 +62,9 @@ system_instruction = """
 6. Լեզուներ՝ ազատ խոսում ես Հայերեն, Ռուսերեն և Անգլերեն:
 """
 
-# Պաշտոնական ճիշտ մոդելի անունը
+# Մոդելի անունը փոխված է 3.6-ի
 model = genai.GenerativeModel(
-    model_name="models/gemini-1.5-flash", system_instruction=system_instruction
+    model_name="gemini-3.6-flash", system_instruction=system_instruction
 )
 
 # --- SIDEBAR (ԿՈՂԱՅԻՆ ՄԵՆՅՈՒ) ---
